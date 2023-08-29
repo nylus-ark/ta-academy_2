3782e7fbd6fa5f6101855147ff32a09099fec744
PAVEL KRASULIN <krasulinpavel1@yandex.ru>
2023-08-29 22:23:06 +0400

    Fix eslint errors

--- ui/homework/homework_1/test1.ts
+++ ui/homework/homework_1/test1.ts
@@ -4,33 +4,33 @@
 */
 
 type Person = {
-  name: string;
-  age: number | string;
-  isSuperHero?: boolean;
-  occupation?: string;
+    name: string;
+    age: number | string;
+    isSuperHero?: boolean;
+    occupation?: string;
 };
 
 const users: Person[] = [
-  {
-    name: 'Owen Willson',
-    age: 54,
-    occupation: 'Actor',
-  },
-  {
-    name: 'Ben Stiller',
-    age: 57,
-    occupation: 'Filmmaker',
-  },
-  {
-    name: 'Batman',
-    age: 'unknown',
-    isSuperHero: true,
-    occupation: 'vigilante',
-  },
+    {
+        name: 'Owen Willson',
+        age: 54,
+        occupation: 'Actor',
+    },
+    {
+        name: 'Ben Stiller',
+        age: 57,
+        occupation: 'Filmmaker',
+    },
+    {
+        name: 'Batman',
+        age: 'unknown',
+        isSuperHero: true,
+        occupation: 'vigilante',
+    },
 ];
 
 function logPerson(user: Person) {
-  console.log(` - ${user.name}, ${user.age}`);
+    console.log(` - ${user.name}, ${user.age}`);
 }
 
 users.forEach(logPerson);
